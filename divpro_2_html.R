a92ddb33b32e5330be8a199b679c81ffedf419c2
Rust Turakulov <turakur@balder.agrf.org.au>
2023-10-06 11:48:06 +1100

    Destination folder and UMAP colors added
    Former-commit-id: 002c3e1d52424cbbf63fef1b9220ea517f1733a3

--- divpro_2_html.R
+++ divpro_2_html.R
@@ -1,4 +1,4 @@
-#!/usr/bin/R
+#!/usr/bin/R                   #6/10/2023
 library(tidyverse)
 library(openxlsx);
 library(vegan);
@@ -9,24 +9,37 @@ library(plotly);
 
 args = commandArgs(trailingOnly = TRUE)
 
-if(length(args) != 1){
-     stop("!!! Crash !!!\n I need path to the DivPro pipeline output directory. Like this:\n/data/Bioinfo/data-proj-rust/fastq_test")
+if(length(args) != 2){
+     stop("!!! Crash !!!\n I need path to the DivPro pipeline output directory and destination folder for the report. Like this:\n
+     /data/Bioinfo/data-proj-rust/fastq_test\n/home/turakur/\n")
 }else{
-     sourcefolder = as.character(args[1])
+     sourcefolder = as.character(args[1]);
      sourcefolder <- paste0("/mnt/", sourcefolder);
+     destinfolder <- as.character(args[2]);
+     destinfolder <- paste0("/mnt/", destinfolder);
+
 }
 
-#setwd("C:/WORKS/DivPro/reporthtml");
 setwd(sourcefolder);
-
-
 mainsummary <-  read.csv("secondary_analysis/overall_summary.tsv", sep="\t");
 dada2       <-  read.csv("secondary_analysis/dada2/DADA2_table.tsv", sep="\t");
-#dadaqcR1    <-  "secondary_analysis/dada2/QC/1_ggsave_1.md.err.png";
-#dadaqcR2    <-  "secondary_analysis/dada2/QC/1_ggsave_1.md.err.png";
+
+dadaqcR1    <-  paste0(sourcefolder, "/secondary_analysis/dada2/QC/1_ggsave_1.md.err.png");
+dadaqcR2    <-  paste0(sourcefolder, "/secondary_analysis/dada2/QC/1_ggsave_2.md.err.png");
+dadaqcR1    <-  gsub("/{2,}", "/", dadaqcR1);
+dadaqcR2    <-  gsub("/{2,}", "/", dadaqcR2);
+qvalpdf1    <-  paste0(sourcefolder, "/secondary_analysis/dada2/QC/FW_qual_stats.pdf");
+qvalpdf2    <-  paste0(sourcefolder, "/secondary_analysis/dada2/QC/RV_qual_stats.pdf");
+qvalpdf1    <-  gsub("/{2,}", "/", qvalpdf1);
+qvalpdf2    <-  gsub("/{2,}", "/", qvalpdf2);
+file.copy(c(dadaqcR1, dadaqcR2, qvalpdf1, qvalpdf2), destinfolder);
+
+
+
 blast       <-  openxlsx::read.xlsx("secondary_analysis/Results/blast/megablast.xlsx", colNames = FALSE);
 blastheader <- c("ASV_ID", "subject_gi", "evalue", "bit_score", "score", "alignment_length", "identity_pcnt", "identical", "positives", "positives_pcnt", "subject_sci_names", "subject_com_names", "subject_blast_names", "subject_super_kingdoms", "subject_title", "sequence");
-names(blast) <- blastheader;
+names(blast)<- blastheader;
+
 DF <- merge(dada2, blast[ ,-16], by="ASV_ID" )
 
 dada <- dada2[,-c(1,ncol(dada2))]; 
@@ -52,9 +65,15 @@ divtable <- data.frame(t(rbind(alphashannon, betasmeanc, uniqasv, totalasv)));
 divtable$alphashannon <- round(divtable$alphashannon, 1);
 divtable$betasmeanc   <- round(divtable$betasmeanc,   1);
 
+setwd(destinfolder);
 file.copy("~/divprobatchreport/divpro_2_html.rmd",  "./");
 file.copy("~/divprobatchreport/agrflogo.png",       "./");
-rmarkdown::render("divpro_2_html.rmd", output_file = "divpro_batchreport.html");
-file.remove("divpro_2_html.rmd");
-file.remove("agrflogo.png");
 
+rmarkdown::render("divpro_2_html.rmd", output_file = "divpro_batchreport.html");
+
+file.remove("divpro_2_html.rmd", 
+            "agrflogo.png",
+	    "1_ggsave_1.md.err.png",
+	    "1_ggsave_2.md.err.png",
+	    "FW_qual_stats.pdf",
+	    "RV_qual_stats.pdf");
